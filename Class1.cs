c2e840e15a3656c485c546b7b7817b17c50d2847
Bruce Chen <bruce68tw@gmail.com>
2021-11-23 19:50:19 +0800

    init

---
+++ Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BaoLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Enums/LaunchStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace BaoLib.Enums
+{
+    /// <summary>
+    /// match DB Bao.LanuchStatus
+    /// </summary>
+    public enum LaunchStatusEnum
+    {
+        No,     //0:未上架
+        Doing,  //1:準備上架
+        Yes,    //2:已上架
+        Over,   //3:下架
+    }
+}
