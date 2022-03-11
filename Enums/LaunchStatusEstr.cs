c0b3856b3e87c0f43c8ab46fcd7a9525ef0ea8d4
Bruce Chen <bruce68tw@gmail.com>
2022-03-11 14:43:51 +0800

    add enum

---
+++ Enums/LaunchStatusEstr.cs
@@ -0,0 +1,13 @@
+﻿namespace BaoLib.Enums
+{
+    /// <summary>
+    /// match DB Bao.LanuchStatus
+    /// </summary>
+    public static class LaunchStatusEstr
+    {
+        public const string No = "0";       //未上架
+        public const string Doing = "1";    //準備上架
+        public const string Yes = "Y";      //已上架
+        public const string Over = "X";     //下架
+    }
+}
---
+++ Enums/RedisTypeEstr.cs
@@ -0,0 +1,11 @@
+﻿namespace BaoLib.Enums
+{
+    /// <summary>
+    /// for redis key
+    /// </summary>
+    public class RedisTypeEstr
+    {
+        public const string BaoList = "BL-";
+        public const string BaoDetail = "BD-";
+    }
+}
